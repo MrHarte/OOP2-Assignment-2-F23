86bb82f041f2b831edf7f759722af20df915cc67
JLac <le2056237"crcmail.com>
2023-09-18 08:43:08 -0400

    Start assignment codes

--- src/main/java/com/champlain/oop2assignment2/Card.java
+++ src/main/java/com/champlain/oop2assignment2/Card.java
@@ -1,6 +1,6 @@
 package com.champlain.oop2assignment2;
 
-public class Card {
+public class Card implements Comparable<Card>{
     private final Suit aSuit;
 
     private final Rank aRank;
@@ -21,5 +21,16 @@ public class Card {
     @Override
     public String toString() {
         return this.aRank + " of " + this.aSuit;
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        int compareRank;
+        compareRank = this.aRank.compareTo(o.getRank());
+
+        if (compareRank == 0) {
+            return this.aSuit.compareTo(o.getSuit());
+        }
+        return compareRank;
     }
 }
--- src/main/java/com/champlain/oop2assignment2/Deck.java
+++ src/main/java/com/champlain/oop2assignment2/Deck.java
@@ -37,4 +37,8 @@ public class Deck implements CardSource {
         }
         return result;
     }
+
+    public void sort(){
+        Collections.sort(aCards);
+    }
 }
