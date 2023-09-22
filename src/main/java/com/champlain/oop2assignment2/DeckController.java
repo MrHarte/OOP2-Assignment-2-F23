2ca025832621a02907760ed2660cc0c4d6d52643
Nikolas34520 <LE2120489@crc-lennox.qc.ca>
2023-09-22 17:29:00 -0400

    Implement Modification

--- src/main/java/com/champlain/oop2assignment2/DeckController.java
+++ src/main/java/com/champlain/oop2assignment2/DeckController.java
@@ -2,6 +2,9 @@ package com.champlain.oop2assignment2;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 
 public class DeckController {
     @FXML
@@ -13,23 +16,50 @@ public class DeckController {
         this.displayDeck();
     }
 
+    /**
+     * Event handler for the Shuffle button.
+     * Shuffles the deck and updates the displayed deck.
+     */
     @FXML
     protected void onShuffleButtonClick() {
         this.aDeck.shuffle();
         this.displayDeck();
     }
 
+    /**
+     * Event handler for the Sort button.
+     * Sorts the deck and updates the displayed deck.
+     */
     @FXML
     protected void onSortButtonClick() {
-        aDeckTextArea.setText("This does not sort anything yet.");
+        aDeck.sort(); // Sort the deck
+        displayDeck(); // Update the text area
     }
 
+
+    /**
+     * Event handler for the Show button.
+     * Draws and displays the next card or shows a message if the deck is empty.
+     */
     @FXML
     protected void onShowButtonClick() {
-        aDeckTextArea.setText("This does not step through anything yet.");
+        if (!aDeck.isEmpty()) {
+            Card nextCard = aDeck.draw(); // Draw the next card
+            String confirmationMessage = nextCard.toString();
+
+            // Show a confirmation dialog
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Card Drawn");
+            alert.setHeaderText(null);
+            alert.setContentText(confirmationMessage);
+            alert.showAndWait();
+        } else {
+            aDeckTextArea.setText("Deck is empty.");
+        }
     }
 
     private void displayDeck () {
         this.aDeckTextArea.setText(this.aDeck.toString());
     }
+
 }
