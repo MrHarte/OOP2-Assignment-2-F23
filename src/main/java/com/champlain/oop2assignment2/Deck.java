09bc4242b9d433cb3034ac4e877463a7c744e54e
Nikolas34520 <LE2120489@crc-lennox.qc.ca>
2023-09-22 17:28:34 -0400

    Implement Modification

--- src/main/java/com/champlain/oop2assignment2/Deck.java
+++ src/main/java/com/champlain/oop2assignment2/Deck.java
@@ -2,9 +2,14 @@ package com.champlain.oop2assignment2;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.List;
 
-public class Deck implements CardSource {
+/**
+ * Constructs a deck of cards by populating it with all possible card combinations.
+ */
+public class Deck implements CardSource, Comparable<Deck>, Iterable<Card> {
     private final List<Card> aCards = new ArrayList<Card>();
 
     public Deck() {
@@ -26,6 +31,14 @@ public class Deck implements CardSource {
         return myCard;
     }
 
+    /**
+     * Sorts the cards in the deck.
+     */
+    public void sort() {
+        Collections.sort(aCards);
+    }
+
+
     public boolean isEmpty() {
         return this.aCards.isEmpty();
     }
@@ -36,5 +49,47 @@ public class Deck implements CardSource {
             result += currentCard.toString() + "\n";
         }
         return result;
+    }
+
+    @Override
+    public int compareTo(Deck otherDeck) {
+        // Compare decks based on the top card they would draw
+        if (this.isEmpty() && otherDeck.isEmpty()) {
+            return 0; // Both decks are empty and considered equal
+        } else if (this.isEmpty()) {
+            return -1; // This deck is empty, so it's considered less than the other
+        } else if (otherDeck.isEmpty()) {
+            return 1; // The other deck is empty, so it's considered less than this one
+        } else {
+            return this.draw().compareTo(otherDeck.draw());
+        }
+    }
+
+    @Override
+    public Iterator<Card> iterator() {
+        return new DeckIterator();
+    }
+
+    private class DeckIterator implements Iterator<Card> {
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < aCards.size();
+        }
+
+        @Override
+        public Card next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return aCards.get(currentIndex++);
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Remove operation is not supported.");
+        }
+
     }
 }
