6313801919ea37a4fe010802e634666263112a78
Murtaza Haidary <LE1983326@crc-lennox.qc.ca>
2023-09-22 13:44:20 -0400

    Modified and added codes in the controller and card

--- src/main/java/com/champlain/oop2assignment2/Card.java
+++ src/main/java/com/champlain/oop2assignment2/Card.java
@@ -1,25 +1,89 @@
+/**
+ * This class holds all the suits and ranks of a card.
+ * IT will also compare the cards
+ */
 package com.champlain.oop2assignment2;
 
-public class Card {
+/**
+ * This holds all the values of a card, such as rank and suit and it also holds the equal/compare
+ */
+public class Card implements Comparable <Card> {
+
     private final Suit aSuit;
 
     private final Rank aRank;
 
+    /**
+     * A card has a rank and a suit.
+     * This methods holds the values in a card such as Suit or Rank
+     * @param pRank equals the Rank in rank class
+     * @param pSuit equals Suit in the suit class
+     */
     public Card (Rank pRank, Suit pSuit) {
         this.aRank = pRank;
         this.aSuit = pSuit;
     }
 
+    /**
+     * Getter method to get the aRank value
+     * @return aRank to get the initiale value.
+     */
     public Rank getRank() {
         return this.aRank;
     }
 
+    /**
+     * Getter method to get the aSuit value
+     * @return aSuit to get the initiale value
+     */
     public Suit getSuit() {
         return this.aSuit;
     }
 
+    /**
+     *  * Returns a string representation of the card.
+     * @return a string value to represent the cards
+     */
     @Override
     public String toString() {
         return this.aRank + " of " + this.aSuit;
     }
+
+    /**
+     * Checks to see if there is any card equal
+     * @param obj obj The object to compare with this card.
+     * @return true if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(!(obj instanceof Card)) return false;
+        return this.aSuit == ((Card) obj).aSuit && this.aRank == ((Card) obj).getRank();
+    }
+
+    /**
+     * Compares this card to another card based on their suits and ranks.
+     * @param o The card to compare to.
+     * @return 0 if the cards are equal, a positive value if this card is greater,
+     *  a negative value if this card is smaller.
+     */
+    @Override
+    public int compareTo(Card o) {
+        if(this.equals(o)) {
+            return 0;
+        }
+        if(this.aSuit.ordinal() > o.getSuit().ordinal()) {
+            return 1;
+        }
+        else if(this.aSuit.ordinal() < o.getSuit().ordinal())
+        {
+            return -1;
+        }
+        if(this.aRank.ordinal() > o.getRank().ordinal()) {
+            return 1;
+        }
+            return -1;
+
+    }
+
 }
--- src/main/java/com/champlain/oop2assignment2/DeckController.java
+++ src/main/java/com/champlain/oop2assignment2/DeckController.java
@@ -1,6 +1,7 @@
 package com.champlain.oop2assignment2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 
 public class DeckController {
@@ -19,14 +20,29 @@ public class DeckController {
         this.displayDeck();
     }
 
+    /**
+     * This sort's the cards inside the deck
+     * and then displays it to the user
+     */
     @FXML
     protected void onSortButtonClick() {
         aDeckTextArea.setText("This does not sort anything yet.");
+        // added code
+        aDeck.sort();
+        displayDeck();
     }
 
+    /**
+     * This method makes it so when you click show/confirm button,
+     * A message box will pop up saying whether the deck is sorted in the way it wants or not.
+     */
     @FXML
     protected void onShowButtonClick() {
-        aDeckTextArea.setText("This does not step through anything yet.");
+        // Show confirmation alert for card
+        for (Card card : this.aDeck) {
+            Alert errorAlert = new Alert(Alert.AlertType.CONFIRMATION, "Confirmation " + card);
+            errorAlert.showAndWait();
+        }
     }
 
     private void displayDeck () {
